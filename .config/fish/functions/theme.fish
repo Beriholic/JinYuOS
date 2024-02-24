2eb6057d217a2be8f62482929435b93899786206
Beriholic <beriholic@petalmail.com>
2024-02-24 23:20:17 +0800

    update

---
+++ .config/fish/functions/theme.fish
@@ -0,0 +1,16 @@
+function theme 
+    switch $argv[1]
+    case light
+         gsettings set org.gnome.desktop.interface  gtk-theme  adw-gtk3 
+         gsettings set org.gnome.desktop.interface color-scheme prefer-light
+         sed -i 's/dark/white/g' $HOME/.config/fcitx5/conf/classicui.conf
+         fcitx5-remote -r
+         echo 'done'
+    case dark
+         gsettings set org.gnome.desktop.interface  gtk-theme  adw-gtk3-dark
+         gsettings set org.gnome.desktop.interface color-scheme prefer-dark
+         sed -i 's/white/dark/g' $HOME/.config/fcitx5/conf/classicui.conf
+         fcitx5-remote -r
+         echo 'done'
+    end
+end

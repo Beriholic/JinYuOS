d825f76084ef40b13336fbf56e41814614ddff0d
Beriholic <beriholic@petalmail.com>
2025-01-24 00:43:58 +0800

    sync

--- .script/screenshot.py
+++ .script/screenshot.py
@@ -1,5 +1,5 @@
 #!/bin/python
-import datetime
+import time
 import os
 
 screenshot_path="$HOME/Pictures/screenshot"
@@ -7,10 +7,8 @@ screenshot_path="$HOME/Pictures/screenshot"
 if not os.path.exists(screenshot_path):
     os.system(f'mkdir -p "{screenshot_path}"')
     
-file_name=str(datetime.datetime.now()).split('.')[0].replace(' ','{')
-file_name="[screenshot]"+file_name[5:]+'}'
-pick_size=os.popen('slurp','r').read().replace("\n","")
+file_name=time.time()
 
-os.system(f'grim -g "{pick_size}" "{screenshot_path}"/"{file_name}.png"')
+os.system(f'grim -g "$(slurp)" ${screenshot_path}/{file_name}.png')
 os.system(f'wl-copy < "{screenshot_path}"/"{file_name}.png"')
 os.system('notify-send -u  low "Shot!"')

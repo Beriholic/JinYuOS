afebd88509763266ae6eef4f143c041a753fb416
Beriholic <beriholic@petalmail.com>
2025-02-13 22:47:28 +0800

    sync

--- .config/hypr/script/adjust-kitty-opactiy.sh
+++ .config/hypr/script/adjust-kitty-opactiy.sh
@@ -2,7 +2,7 @@
 
 # For Hyprland
 if [[ $XDG_CURRENT_DESKTOP == "Hyprland" ]]; then
-  sed -i 's/^background_opacity.*$/background_opacity 0.8/' ~/.config/kitty/kitty.conf
+  sed -i 's/^background_opacity.*$/background_opacity 0.5/' ~/.config/kitty/kitty.conf
 fi
 
 # For KDE

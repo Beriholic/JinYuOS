83eb96f8e427b4e8fb97ac21ec23d05cd38c7b20
Beriholic <beriholic@petalmail.com>
2025-04-20 00:37:11 +0800

    add fonts and configure fontconfig

--- host/fonts.nix
+++ host/fonts.nix
@@ -4,19 +4,32 @@
 }:
 
 {
-  fonts.packages = with pkgs; [
-    noto-fonts
-    noto-fonts-cjk-sans
-    noto-fonts-cjk-serif
-    noto-fonts-emoji
-    nerdfonts
-    # nerd-fonts.agave  
-    # nerd-fonts._0xproto
-    material-symbols
-    google-fonts
-    jetbrains-mono
-    google-fonts
-    cascadia-code
-    material-symbols
-  ];
+  fonts = {
+    packages = with pkgs; [
+      noto-fonts
+      noto-fonts-cjk-sans
+      noto-fonts-cjk-serif
+      noto-fonts-emoji
+      fira-code-nerdfont
+      nerdfonts
+      # nerd-fonts.agave
+      # nerd-fonts._0xproto
+      material-symbols
+      google-fonts
+      jetbrains-mono
+      google-fonts
+      cascadia-code
+      material-symbols
+    ];
+    fontconfig = {
+      antialias = true;
+      hinting.enable = true;
+      defaultFonts = {
+        emoji = [ "Noto Color Emoji" ];
+        monospace = [ "FiraCode Nerd Font" ];
+        sansSerif = [ "Noto Sans CJK SC" ];
+        serif = [ "Noto Serif CJK SC" ];
+      };
+    };
+  };
 }

c72b62473a9f505eb0725270d5990984402a3deb
Beriholic <beriholic@petalmail.com>
2025-06-03 17:47:21 +0800

    chore :hammer:: rename services and enable some settings

--- nixos/services.nix
+++ nixos/services.nix
@@ -2,16 +2,15 @@
   services = {
     udisks2.enable = true;
     upower.enable = true;
+    libinput.enable = true;
+    power-profiles-daemon.enable = true;
+    displayManager.sessionPackages = [ pkgs.hyprland ];
 
     xserver = {
       enable = true;
       desktopManager.gnome.enable = true;
       displayManager.gdm.enable = true;
       displayManager.gdm.wayland = true;
-      displayManager.sessionPackages = [ pkgs.hyprland ];
-      libinput.enable = true;
     };
-
-    power-profiles-daemon.enable = true;
   };
 }

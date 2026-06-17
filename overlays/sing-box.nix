cec8d9f4dcfa77bb7dea96a7f23dc48ac6dd1e7e
Beriholic <beriholic@petalmail.com>
2026-06-17 09:24:45 +0800

    feat✨(overlays): add sing-box 1.13.12 overlay

---
+++ overlays/sing-box.nix
@@ -0,0 +1,12 @@
+final: prev: {
+  sing-box = prev.sing-box.overrideAttrs (oldAttrs: rec {
+    version = "1.13.12";
+    src = prev.fetchFromGitHub {
+      owner = "SagerNet";
+      repo = "sing-box";
+      tag = "v${version}";
+      hash = "sha256-AGv/0zUYWD32mQMkdiltWjMzHjfiAGIC1QDqZpK5sCw=";
+    };
+    vendorHash = "sha256-lA7EuNsjCWlgZZNc/fSEnNQz2pX8jCqy12tukrBs8j8=";
+  });
+}

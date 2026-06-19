0c6f9a72b7f60e66d1ad3c1e1b4da171b7782c67
Beriholic <hi@beriholic.cv>
2026-06-19 21:16:55 +0800

    chore🔧: remove surge-server launchd daemon

--- darwin/services.nix
+++ darwin/services.nix
@@ -12,15 +12,4 @@
       StandardErrorPath = "/var/log/sing-box-error.log";
     };
   };
-
-  launchd.daemons.surge-server = {
-    command = "${pkgs.surge}/bin/surge server";
-    serviceConfig = {
-      RunAtLoad = true;
-      KeepAlive = true;
-      UserName = "beriholic";
-      StandardOutPath = "/Users/beriholic/Library/Logs/surge-server.log";
-      StandardErrorPath = "/Users/beriholic/Library/Logs/surge-server-error.log";
-    };
-  };
 }
